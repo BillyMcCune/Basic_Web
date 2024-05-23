1dd7ee655eb819d3168775bd7b3d297e23283dba
BillyMcCune <143638147+BillyMcCune@users.noreply.github.com>
2024-05-23 12:33:39 -0400

    Added Image

--- sketch.js
+++ sketch.js
@@ -1,29 +1,31 @@
 var chick;
+
 function setup() {
-  createCanvas(1000, 400);
-  chick = new Chicken;
+  canvas = createCanvas(1000, 400);
+  chick = new Chicken();
 }
 
 function draw() {
-  background(0,100,200);
+  background(0, 100, 200);
   chick.drawc();
-  if(frameCount > 100)
-  chick.chickenWOAH();
-  
+  if (frameCount > 100) {
+    chick.chickenWOAH();
+  }
 }
 
- function Chicken() {
+function Chicken() {
   this.black = 0;
   this.beak = 1;
   this.gobble = 3;
-  this.x = width/2;
-  this.y = height/2;
+  this.x = width / 2;
+  this.y = height / 2;
   this.drawc = function() {
-    fill(80,100,160);
-    circle(this.x,this.y,this.beak)};
+    fill(80, 100, 160);
+    circle(this.x, this.y, this.beak);
+  };
   this.chickenWOAH = function() {
-    if(this.beak < 250){
-      this.beak +=1;
+    if (this.beak < 250) {
+      this.beak += 1;
     }
-  }
-};
+  };
+}
