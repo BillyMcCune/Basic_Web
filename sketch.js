bf283ba014272c0dc75ed99319804ed31d424c23
BillyMcCune <143638147+BillyMcCune@users.noreply.github.com>
2024-05-23 12:08:50 -0400

    Changes p5

--- sketch.js
+++ sketch.js
@@ -1,7 +1,9 @@
 function setup() {
-  createCanvas(400, 400);
-  background(220);
+  createCanvas(windowWidth, WindowHeight/2);
+  background(0,100,200);
 }
 function draw() {
+  fill(255, 0, 0);
+  noStroke();
   ellipse(200, 200, 50, 50);
 }
