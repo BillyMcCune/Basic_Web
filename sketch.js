3ac6f254645625a0a44b71be8314f2477c139a2e
BillyMcCune <143638147+BillyMcCune@users.noreply.github.com>
2024-05-23 12:12:43 -0400

    added chick

--- sketch.js
+++ sketch.js
@@ -1,9 +1,29 @@
+var chick;
 function setup() {
-  createCanvas(400, 800);
+  createCanvas(400, 400);
+  chick = new Chicken;
 }
+
 function draw() {
   background(0,100,200);
-  fill(255, 0, 0);
-  noStroke();
-  ellipse(200, 200, 50, 50);
+  chick.drawc();
+  if(frameCount > 100)
+  chick.chickenWOAH();
+  
 }
+
+ function Chicken() {
+  this.black = 0;
+  this.beak = 1;
+  this.gobble = 3;
+  this.x = width/2;
+  this.y = height/2;
+  this.drawc = function() {
+    fill(80,100,160);
+    circle(this.x,this.y,this.beak)};
+  this.chickenWOAH = function() {
+    if(this.beak < 250){
+      this.beak +=1;
+    }
+  }
+};
